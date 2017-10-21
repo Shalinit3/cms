6f3e2431a2867875c78d6a85fef4f1a30fabb374
deepanshut041 <deepanshut041@gmail.com>
2017-10-21 20:47:20 +0530

    add log styling done

--- add_logo.php
+++ add_logo.php
@@ -16,36 +16,57 @@
 
 <body>
 <div>
-    <?php include("include/header.php");
-    include("include/sidebar.php");?>
-    <article class="container-fluid" id="main-container">
-        <section>
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    Add Logo Image
-                    <form action="control/add_logo.php" method="post" enctype="multipart/form-data">
-                        Select image to upload:
-                        <input type="file" name="fileToUpload" id="fileToUpload">
-                        <img id="preview" src="#"  style="height:100px;width:100px" />
-                        <input type="submit" value="Upload Image" name="submit">
-                    </form>
-                </div>
-            </div>
-        </section>
-        <section>
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <?php $logo= mysqli_query($con,"Select * from `logo` ");
-                                while($data=mysqli_fetch_array($logo)) { ?>
-                                    <div class="col-md-3">
-                                        <img src="control/<?php echo $data['image']; ?>"  />
+    <?php include("include/header.php");?>
+    <?php include("include/sidebar.php");?>
+    <article id="main-container">
+        <div id="add_logo_container" class="row align-self-center">
+            <div id="add_logo" class="col-11 col-md-10 col-lg-8">
+                <section>
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <h3 class="text-center">Add Logo Image</h3>
+                            <hr>
+                            <form action="control/add_logo.php" method="post" enctype="multipart/form-data">
+                                <h6>Select image to upload:</h6>
+                                <div id="preview-container" class="row align-self-center">
+                                    <img id="preview" src="http://via.placeholder.com/400x400" class="img-fluid" />
+                                </div>
+                                <br>
+                                <div class="button-container row">
+                                    <div class="choose-button col text-right">
+                                        <label for="fileToUpload" class="btn btn-info">Choose File</label>
+                                        <input type="file" name="fileToUpload" id="fileToUpload">
                                     </div>
-                        <?php } ?>
+                                    <div class="col text-left">
+                                        <button type="submit" name="submit" class="btn btn-warning">Upload Image</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                </section>
+                <hr>
+                <section id="img-scroll">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="row">
+                                <?php $logo= mysqli_query($con,"Select * from `logo` ");
+                                while($data=mysqli_fetch_array($logo)) { ?>
+                                    <div class="col-md-4 col-lg-3 col-12">
+                                        <div class="img-container">
+                                            <img class="img-fluid mx-auto d-block" src="control/<?php echo $data['image']; ?>"  />
+                                            <div class="text-center">
+                                                <i class="fa fa-trash-o fa-2x"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-        </section>
+        </div>
     </article>
 </div>
 <script>
